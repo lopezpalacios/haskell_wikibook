36a27404841663de1219fbbea7e542fe1b284b37
Jesus Lopez Palacios <jesuslopac@Jesuss-MacBook-Air.local>
2022-12-28 16:10:23 -0600

    adding heron triangle to functions.hs script

--- variables_and_functions/functions.hs
+++ variables_and_functions/functions.hs
@@ -1,4 +1,4 @@
-area r = pi * r ^ 2 
+areaCircle r = pi * r ^ 2 
 {--
 ^ name of the function comes first
      ^ arguments come next
@@ -15,3 +15,9 @@ areaRect w h = w * h
 areaSquare s = areaRect s s
 areaTriangle b h = (b * h) / 2 
 minus x y = x - y 
+volumeBox l w h = (areaRect l w) * h
+volumeCylinder r h = (areaCircle r) * h 
+--Heron's formula
+heron a b c = sqrt(s * (s - a)*(s - b)*(s - c))
+    where
+    s = (a + b + c)/2
