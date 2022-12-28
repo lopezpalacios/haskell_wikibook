b9ba43cd27dd16136557dcb0cf9502aebb2b031f
Jesus Lopez Palacios <jesuslopac@Jesuss-MacBook-Air.local>
2022-12-28 15:54:16 -0600

    adding functions script

---
+++ variables_and_functions/Varfun.hs
@@ -0,0 +1,8 @@
+--this is a way to load variables inside of a haskell source file
+--first we will define the value of a radius
+r = 5.0
+--and then the area of a circle
+area = pi * r^2
+{--
+and this is how we make multi-line comments
+--}
---
+++ variables_and_functions/functions.hs
@@ -0,0 +1,17 @@
+area r = pi * r ^ 2 
+{--
+^ name of the function comes first
+     ^ arguments come next
+        ^ equal sign
+          ^  function definition
+--}
+--evaluations
+double x = 2 * x 
+quadruple x = double (double x)
+square x = x * x
+half x = x / 2
+--are of rectagle
+areaRect w h = w * h
+areaSquare s = areaRect s s
+areaTriangle b h = (b * h) / 2 
+minus x y = x - y 
